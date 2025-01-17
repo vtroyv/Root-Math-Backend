73dd834a95ed193f5880acf590ed80f9e851ea43
vtroyv <troycampbell11@aol.com>
2025-01-17 17:54:25 +0000

    Recieve feedback from gpt-api and return to client, currently no function calling

--- app/routers/feedback.py
+++ app/routers/feedback.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 from typing import List
 from ..utils.preprocess_sympy import preprocess_sympy
+from ..utils.llm import grade_feedback
 from sympy import sympify
 
 app = FastAPI()
@@ -16,8 +17,19 @@ router = APIRouter(prefix='/feedback')
 async def feedback(response: StudentResponse):
     print(f'the initial response is {response}')
     data = preprocess_sympy(response.sympy)
+    
+    print(f'The original data is {data}')
     print(f"The data is given by the following{data['meta_data']['response']}")
+    
+    feedbackData = {
+        "usersSympyResponse": data['meta_data']['response'],
+        "questionData": response.questionData
+    }
+    
    
-    return {"feedback": "Good Job"}
+    print(f"The data i want to send to llm is {feedbackData}")
+    feedback = grade_feedback(feedbackData)
+   
+    return {"feedback": feedback}
 
 app.include_router(router)
